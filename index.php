306e4c4f466a648c7f7f2e01955cfff297c4334f
bign8 <big.nate.w@gmail.com>
2014-03-25 16:54:03 -0600

    Initial Commit

---
+++ index.php
@@ -0,0 +1,234 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>ELA - App</title>
+	<link href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet">
+	<style>
+		/* Sticky footer styles
+		-------------------------------------------------- */
+		html, body { height: 100%; } /* The html and body elements cannot have any padding or margin. */
+		#wrap { /* Wrapper for page content to push down footer */
+		  min-height: 100%;
+		  height: auto !important;
+		  height: 100%;
+		  margin: 0 auto -60px; /* Negative indent footer by its height */
+		  /*padding: 0 0 60px;  Pad bottom by footer height */
+		}
+		#footer { /* Set the fixed height of the footer here */
+		  height: 60px;
+		  background-color: #f5f5f5;
+		}
+
+		/* Custom page CSS
+		-------------------------------------------------- */
+		/* Not required for template or sticky footer method. */
+		#wrap > .container { padding: 60px 15px 0; }
+		.container .credit { margin: 20px 0; }
+		#footer > .container { padding-left: 15px; padding-right: 15px; }
+		code { font-size: 80%; }
+
+		/* Nate page CSS
+		-------------------------------------------------- */
+		/*#wrap > .container { min-height: 600px; }*/
+		img.pull-right { padding: 0; margin: 0 0 1em 1em; }
+		.center-cropped {
+		    width: 75px;
+		    height: 100px;
+		    background-position: center center;
+		    background-repeat: no-repeat;
+		    overflow: hidden;
+		    border-radius: 10px;
+		}
+
+		/* Set the image to fill its parent and make transparent */
+		.center-cropped img {
+		   min-height: 100px;
+		   min-width: 100px;
+		   -ms-filter:"progid:DXImageTransform.Microsoft.Alpha(Opacity=0)"; /* IE 8 */
+		   filter: alpha(opacity=0); /* IE 5-7 */
+		   opacity: 0; /* modern browsers */
+		}
+	</style>
+</head>
+<body data-spy="scroll" data-target=".navbar">
+	<!-- Wrap all page content here -->
+	<div id="wrap" data-ng-app="ela">
+
+		<!-- Fixed navbar -->
+		<div class="navbar navbar-default navbar-fixed-top">
+			<div class="container">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="#">ELA</a>
+				</div>
+				<div class="collapse navbar-collapse">
+					<ul class="nav navbar-nav">
+						<li class="active"><a href="#home">Home</a></li>
+						<li><a href="#about">About</a></li>
+						<li><a href="#contacts">Contacts</a></li>
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+							<ul class="dropdown-menu">
+								<li><a href="#">Action</a></li>
+								<li><a href="#">Another action</a></li>
+								<li><a href="#">Something else here</a></li>
+								<li class="divider"></li>
+								<li class="dropdown-header">Nav header</li>
+								<li><a href="#">Separated link</a></li>
+								<li><a href="#">One more separated link</a></li>
+							</ul>
+						</li>
+					</ul>
+				</div><!--/.nav-collapse -->
+			</div>
+		</div>
+
+		<!-- Begin page content -->
+		<div class="container" id="home">
+			<div class="page-header">
+				<h1>Welcome to ELA</h1>
+			</div>
+			<img class="pull-right img-responsive col-sm-3 hidden-xs img-rounded" src="http://lorempixel.com/g/300/500/business/Placeholder?1">
+			<p class="lead">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac eleifend nisl. Mauris vitae tincidunt turpis, quis facilisis orci. Maecenas elementum sapien vitae libero semper commodo. Quisque tincidunt, dui vitae laoreet sollicitudin, sem tellus interdum odio, eu ornare tellus tellus ac tortor. Nulla laoreet urna ligula, ut ultrices dui ultricies et. Proin in viverra tellus. Duis vel sem quis neque molestie vehicula id eu mauris. Mauris eleifend ut est at luctus. Sed nec ligula a lectus ultricies bibendum viverra vitae ante. Suspendisse fringilla turpis nisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec fermentum nisl urna, non dictum dolor facilisis eu.
+			</p>
+			<p>
+				In feugiat tincidunt tortor. Quisque cursus purus a justo egestas, id consectetur tellus vulputate. Quisque sed blandit nulla. Nunc fringilla quis lacus eget hendrerit. Etiam tincidunt metus at nisl scelerisque dapibus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus vestibulum mauris lacus, nec accumsan lectus adipiscing nec. Vestibulum convallis suscipit lectus vitae eleifend. Proin luctus pulvinar mi et elementum. Pellentesque luctus, metus quis elementum dictum, leo lorem posuere odio, vel accumsan tellus odio at risus. Proin tempus imperdiet quam nec vestibulum.
+			</p>
+			<p>
+				Morbi placerat felis neque, at placerat lorem tempus quis. Duis tincidunt tempor nisl sed tempus. Etiam a pharetra enim. Ut semper in quam quis sodales. In ac velit viverra, tristique lectus ut, imperdiet nulla. Morbi egestas dapibus turpis at sodales. Ut imperdiet turpis justo, ac placerat risus gravida sed. Suspendisse potenti.
+			</p>
+			<p>
+				Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Mauris nisl sem, fringilla nec neque molestie, porttitor accumsan tellus. Nam felis odio, sollicitudin a mi vitae, vehicula dictum diam. Cras condimentum sit amet lacus at luctus. Curabitur aliquet ante consequat, aliquet ligula eu, congue nisl. Proin nibh ligula, ornare vel elit ac, sagittis lacinia tellus. Duis non orci egestas, pharetra purus id, mattis nisi. Maecenas sagittis, erat id placerat placerat, ante est rhoncus ligula, eget aliquet tellus odio vitae augue. Cras non mauris neque. Cras varius vestibulum dui sed rutrum. Maecenas nisl est, pharetra sit amet risus eget, ornare scelerisque lorem. Nunc at dolor non odio ullamcorper vulputate vitae a purus.
+			</p>
+			<p>
+				Quisque semper varius nisl vel facilisis. Maecenas ornare molestie diam vitae ultrices. Donec vel erat quis magna tempor pharetra. Quisque congue lorem sit amet ultrices cursus. Suspendisse turpis sapien, mattis ac augue eget, varius sollicitudin dui. Mauris vel justo in nulla convallis aliquam ut vel elit. Sed pretium rhoncus imperdiet. Ut lacus erat, suscipit quis enim et, iaculis dictum ipsum. Vestibulum dignissim, lacus vel aliquet molestie, urna purus cursus libero, in lacinia velit leo a enim. Ut et interdum magna, sit amet placerat erat. Praesent nec tempor neque. Cras ultricies odio a dui fringilla consequat. Donec dictum consequat eros quis lobortis. Proin venenatis lacus et sem congue, at placerat velit consectetur. Nunc lobortis est a dolor varius, non vestibulum elit feugiat.
+			</p>
+		</div>
+
+		<!-- Begin page content -->
+		<div class="container" id="about">
+			<div class="page-header">
+				<h1>About</h1>
+			</div>
+			<img class="pull-right img-responsive col-sm-3 hidden-xs img-rounded" src="http://lorempixel.com/g/300/500/business/Placeholder?2">
+			<p class="lead">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac eleifend nisl. Mauris vitae tincidunt turpis, quis facilisis orci. Maecenas elementum sapien vitae libero semper commodo. Quisque tincidunt, dui vitae laoreet sollicitudin, sem tellus interdum odio, eu ornare tellus tellus ac tortor. Nulla laoreet urna ligula, ut ultrices dui ultricies et. Proin in viverra tellus. Duis vel sem quis neque molestie vehicula id eu mauris. Mauris eleifend ut est at luctus. Sed nec ligula a lectus ultricies bibendum viverra vitae ante. Suspendisse fringilla turpis nisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec fermentum nisl urna, non dictum dolor facilisis eu.
+			</p>
+			<p>
+				In feugiat tincidunt tortor. Quisque cursus purus a justo egestas, id consectetur tellus vulputate. Quisque sed blandit nulla. Nunc fringilla quis lacus eget hendrerit. Etiam tincidunt metus at nisl scelerisque dapibus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus vestibulum mauris lacus, nec accumsan lectus adipiscing nec. Vestibulum convallis suscipit lectus vitae eleifend. Proin luctus pulvinar mi et elementum. Pellentesque luctus, metus quis elementum dictum, leo lorem posuere odio, vel accumsan tellus odio at risus. Proin tempus imperdiet quam nec vestibulum.
+			</p>
+			<p>
+				Morbi placerat felis neque, at placerat lorem tempus quis. Duis tincidunt tempor nisl sed tempus. Etiam a pharetra enim. Ut semper in quam quis sodales. In ac velit viverra, tristique lectus ut, imperdiet nulla. Morbi egestas dapibus turpis at sodales. Ut imperdiet turpis justo, ac placerat risus gravida sed. Suspendisse potenti.
+			</p>
+			<p>
+				Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Mauris nisl sem, fringilla nec neque molestie, porttitor accumsan tellus. Nam felis odio, sollicitudin a mi vitae, vehicula dictum diam. Cras condimentum sit amet lacus at luctus. Curabitur aliquet ante consequat, aliquet ligula eu, congue nisl. Proin nibh ligula, ornare vel elit ac, sagittis lacinia tellus. Duis non orci egestas, pharetra purus id, mattis nisi. Maecenas sagittis, erat id placerat placerat, ante est rhoncus ligula, eget aliquet tellus odio vitae augue. Cras non mauris neque. Cras varius vestibulum dui sed rutrum. Maecenas nisl est, pharetra sit amet risus eget, ornare scelerisque lorem. Nunc at dolor non odio ullamcorper vulputate vitae a purus.
+			</p>
+			<p>
+				Quisque semper varius nisl vel facilisis. Maecenas ornare molestie diam vitae ultrices. Donec vel erat quis magna tempor pharetra. Quisque congue lorem sit amet ultrices cursus. Suspendisse turpis sapien, mattis ac augue eget, varius sollicitudin dui. Mauris vel justo in nulla convallis aliquam ut vel elit. Sed pretium rhoncus imperdiet. Ut lacus erat, suscipit quis enim et, iaculis dictum ipsum. Vestibulum dignissim, lacus vel aliquet molestie, urna purus cursus libero, in lacinia velit leo a enim. Ut et interdum magna, sit amet placerat erat. Praesent nec tempor neque. Cras ultricies odio a dui fringilla consequat. Donec dictum consequat eros quis lobortis. Proin venenatis lacus et sem congue, at placerat velit consectetur. Nunc lobortis est a dolor varius, non vestibulum elit feugiat.
+			</p>
+		</div>
+
+		<!-- Begin page content -->
+		<div class="container" id="contacts" data-ng-controller="search">
+			<div class="page-header">
+				<h1>Contacts</h1>
+			</div>
+			<p class="lead">
+				Find your friends and colleagues from ELA right here.
+			</p>
+
+			<div class="row">
+				<div class="col-md-7 col-sm-8"><!-- search -->
+					<div class="input-group">
+						<label class="input-group-addon" for="search_field">Search</label>
+						<input type="text" class="form-control" id="search_field" placeholder="Type here..." data-ng-model="search_str">
+						<span class="input-group-btn">
+							<button class="btn btn-danger" data-ng-click="search_str=''">
+								<i class="glyphicon glyphicon-remove" style="top:2px"></i>
+							</button>
+						</span>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-4 hidden-xs"><!-- order -->
+					<div class="input-group">
+						<label class="input-group-addon" for="search_order">Order</label>
+						<select ng-model="field" class="form-control" id="search_order" 
+							data-ng-options="x.field as x.disp for x in fields" 
+							data-ng-change="sort_order=false">
+							<option value="" disabled>Order By...</option>
+						</select>
+						<span class="input-group-btn">
+							<button class="btn btn-info" data-ng-click="sort_order=!sort_order">
+								<i class="glyphicon" data-ng-class="{'glyphicon-chevron-down':sort_order, 'glyphicon-chevron-up':!sort_order}"></i>
+							</button>
+						</span>
+					</div>
+				</div>
+				<div class="col-md-2 hidden-xs hidden-sm"><!-- pagination -->
+					<div class="input-group">
+						<label class="input-group-addon" for="search_page">Limit</label>
+						<select ng-model="limit" class="form-control" id="search_page" data-ng-options="x for x in limits">
+							<option value="" disabled>Per Page...</option>
+						</select>
+				</div>
+			</div>
+
+			<table class="table table-striped">
+				<thead>
+					<tr>
+						<th>Contact</th>
+						<th style="width:70%" class="hidden-xs">
+							<small class="pull-right text-right" style="font-weight:normal" data-ng-bind="total_rows()"></small>
+							Bio
+						</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr ng-repeat="user in (filtered_users = (users | filter:search_str)) | orderBy:field:sort_order | pagination:page:limit">
+						<td>
+							<!-- <div class="center-cropped pull-right" style="background-image: url('img/{{user.Last}}, {{user.First}}.jpg');"> -->
+								<img data-ng-src="img/{{user.Last}}, {{user.First}}.jpg" class="img-rounded pull-left" height="100px" width="75px"
+									data-ng-attr-title="{{user.First}} {{user.Last}}"
+									data-ng-attr-alt="{{user.First}} {{user.Last}}" style="margin-right: .5em" />
+							<!-- </div> -->
+							<strong>
+								<span data-ng-bind="user.First">John</span>&nbsp;
+								<span data-ng-bind="user.Last">Doe</span>
+							</strong><br/>
+							<small data-ng-bind="user.TITLE">Intern</small><br/>
+							<span class="text-muted">
+								<span data-ng-bind="user.COMPANY">Temporary INC.</span><br/>
+								<span data-ng-bind="user.CITY">Two Dot</span>,&nbsp;<span data-ng-bind="user.STATE">MT</span>
+							</span>
+						</td>
+						<td data-ng-bind="user.Bio" class="hidden-xs">was an amazing ...</td>
+					</tr>
+				</tbody>
+			</table>
+
+			<div class="text-center" data-ng-show="pages() !== 1"><!-- pager -->
+				<pagination num-pages="pages()" current-page="page" max-show="5"></pagination>
+			</div>
+		</div>
+	</div>
+
+	<div id="footer">
+		<div class="container">
+			<p class="text-muted credit">
+				&copy; <a href="http://upstreamacademy.com">Upstream Academy</a> 
+				<script>document.write(new Date().getFullYear())</script>
+			</p>
+		</div>
+	</div>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+	<script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.14/angular.min.js"></script>
+	<script src="js.js"></script>
+</body>
+</html>
