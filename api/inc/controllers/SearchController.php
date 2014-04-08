b3393130a9d6a27b5abdf4b583be28e5df8e015c
bign8 <big.nate.w@gmail.com>
2014-04-08 12:33:39 -0600

    Cleaning up past APIs

--- api/inc/controllers/SearchController.php
+++ api/inc/controllers/SearchController.php
@@ -6,7 +6,7 @@ class SearchController extends ApiController {
 
 		$data = array();
 		if ($action == '') {
-			$sth  = $this->db->prepare("SELECT * FROM user p LEFT JOIN (SELECT * FROM note WHERE srcID=?) n ON p.accountno = n.destID;");
+			$sth  = $this->db->prepare("SELECT accountno, first, last, company, title, city, state, bio, gradYear, phone, email, user, noteID, srcID, destID, note FROM user p LEFT JOIN (SELECT * FROM note WHERE srcID=?) n ON p.accountno = n.destID;");
 			$sth->execute( array( $this->user->accountno ) );
 			$data = $sth->fetchAll( PDO::FETCH_ASSOC );
 		} else {
