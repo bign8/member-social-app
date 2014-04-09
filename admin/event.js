6e236e178ecc3ffe58e87349cec5f3478808f8b9
bign8 <big.nate.w@gmail.com>
2014-04-09 16:23:26 -0600

    Adding event managing page

---
+++ admin/event.js
@@ -0,0 +1,99 @@
+angular.module('ela-event', []).
+
+controller('event-edit', ['$scope', 'API', function ($scope, API) {
+
+	// Program Years
+	$scope.prgmYears = [];
+	var PrgmYear = new API('programYear', 'programYearID');
+	PrgmYear.all().then(function (data) {
+		$scope.prgmYears = data;
+		$scope.activePrgmYear = data[data.length-1]; // set default as last
+	});
+
+	// Years
+	$scope.years = [];
+	var Year = new API('year', 'yearID');
+	Year.all().then(function (data) { $scope.years = data; });
+
+	// Events
+	$scope.events = [];
+	var Event = new API('event', 'eventID');
+	Event.all().then(function (data) { $scope.events = data; });
+
+	// Save Controllers
+	$scope.saveYear = Year.set.bind(Year);
+	$scope.saveEvent = Event.set.bind(Event);
+}]).
+
+factory('API', ['$http', function ($http) { // TODO: improve with browser data cashe
+	var cleanup = function (result) { return result.data; }
+	var service = function(table, identifier) {
+		this.base = table;
+		this.id = identifier;
+	};
+	service.prototype = {
+		all: function () {
+			return $http.get('db/' + this.base).then( cleanup );
+		},
+		get: function (itemID) {
+			return $http.get('db/' + this.base + '/' + itemID).then( cleanup );
+		},
+		set: function (item) {
+			return $http.put('db/' + this.base + '/' + item[ this.id ], item).then( cleanup );
+		},
+		rem: function (item) {
+			return $http.delete('db/' + this.base + '/' + item[ this.id ]).then( cleanup );
+		},
+		add: function (item) {
+			return $http.post('db/' + this.base, item).then( cleanup );
+		}
+	};
+	return service;
+}]).
+
+directive('colEditor', function () {
+	return {
+		replace: true,
+		scope: {
+			colField: '=',
+			saveCb: '&'
+		},
+		templateUrl: 'event.colEditor.tpl.html',
+		link: function (scope, elem, attrs) {
+			var origional = null;
+			scope.active = false;
+			scope.start_editing = function () {
+				origional = angular.copy(scope.colField);
+				scope.active = true;
+			};
+			scope.done_editing = function () {
+				if (scope.active && scope.colField != origional) scope.saveCb();
+				scope.active = false;
+			};
+			scope.undo_editing = function () {
+				scope.colField = origional;
+				scope.done_editing();
+			};
+		}
+	};
+}).
+
+directive('editEscape', function () {
+	var ESCAPE_KEY = 27;
+	return function (scope, elem, attrs) {
+		elem.bind('keydown', function (event) {
+			if (event.keyCode === ESCAPE_KEY) 
+				scope.$apply(attrs.editEscape);
+		});
+	};
+}).
+
+directive('editFocus', ['$timeout', function ($timeout) {
+	return function (scope, elem, attrs) {
+		scope.$watch(attrs.editFocus, function (newVal) {
+			if (newVal) $timeout(function () {
+				elem[0].focus();
+			}, 0, false);
+		});
+	};
+}]);
---
+++ admin/event.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Quote Editor</title>
+	<link href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body data-ng-app="ela-event">
+	
+	<div class="container" data-ng-controller="event-edit" data-ng-cloak>
+		<div class="col-md-12">
+			<div class="col-md-6" style="position:absolute;bottom:0;right:0">
+				<select 
+					class="form-control" 
+					data-ng-options="v.programYear for v in prgmYears | orderBy:'programYear'" 
+					data-ng-model="activePrgmYear"
+				>
+					<option value="" disabled> -- CHOOSE ONE -- </option>
+				</select>
+			</div>
+			<h2>Program Years <small>Choose one</small></h2>
+		</div>
+		<div class="col-md-6">
+			<h2>Year In Program <small ng-show="activePrgmYear.programYear">During {{activePrgmYear.programYear}}</small></h2>
+			<table class="table" ng-show="filteredYears.length">
+				<thead>
+					<tr>
+						<th class="col-xs-2">Year</th>
+						<th class="col-xs-5">Workbook</th>
+						<th class="col-xs-5">Calendar</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr data-ng-repeat=" year in filteredYears = (years | filter:{programYearID:activePrgmYear.programYearID}) | orderBy:'year' ">
+						<td>Year {{ year.year }}</td>
+						<td data-col-editor data-col-field="year.workbook" data-save-cb="saveYear(year)">-</td>
+						<td data-col-editor data-col-field="year.calendar" data-save-cb="saveYear(year)">-</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+		<div class="col-md-6">
+			<h2>Events <small ng-show="activePrgmYear.programYear">In {{activePrgmYear.programYear}}</small></h2>
+			<table class="table" ng-show="filteredEvents.length">
+				<thead>
+					<tr>
+						<th class="col-xs-3">Name</th>
+						<th class="col-xs-4">Date</th>
+						<th class="col-xs-5">Link</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr data-ng-repeat=" event in filteredEvents = (events | filter:{programYearID:activePrgmYear.programYearID}) | orderBy:'name' ">
+						<td>{{ event.name }}</td>
+						<td data-col-editor data-col-field="event.date" data-save-cb="saveEvent(event)">-</td>
+						<td data-col-editor data-col-field="event.link" data-save-cb="saveEvent(event)">-</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	</div>
+
+	<script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.14/angular.min.js"></script>
+	<script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.14/angular-resource.min.js"></script>
+	<script src="event.js"></script>
+</body>
+</html>
