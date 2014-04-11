4e31a3a03adfafb8e8551bf3e0e2c9523f2ac7aa
bign8 <big.nate.w@gmail.com>
2014-04-11 13:05:28 -0600

    Improve administrative login

--- admin/login.php
+++ admin/login.php
@@ -1,25 +1,32 @@
 <?php
 	require_once('admin.class.php');
 	include('tpl' . DIRECTORY_SEPARATOR . 'header.tpl.html');
-
 	if ($admin->login_status) die(header('Location: index.php'));
-	if ($admin->login_status === false) echo 'login error';
 ?>
 
 <div class="page-header clearfix">
 	<h2>ELA Admin Login</h2>
 </div>
 
-<form role="form" method="post">
-	<div class="form-group">
-		<label for="user">Username</label>
-		<input type="text" class="form-control" id="user" placeholder="Username" name="user">
-	</div>
-	<div class="form-group">
-		<label for="pass">Password</label>
-		<input type="password" class="form-control" id="pass" placeholder="Password" name="pass">
-	</div>
-	<button type="submit" class="btn btn-default" name="action" value="login">Submit</button>
-</form>
+<div class="col-md-6 col-md-offset-3">
+	<?php if ($admin->login_status === false): ?>
+		<div class="alert alert-danger">
+			<strong>Oh snap!</strong> Change a few things up and try authenticating again.
+		</div>
+	<?php endif; ?>
+	<form role="form" method="post">
+		<div class="form-group">
+			<label for="user">Username</label>
+			<input autofocus type="text" class="form-control" id="user" placeholder="Username" name="user" 
+				value="<?php echo isset($_REQUEST['user']) ? $_REQUEST['user'] : ''; ?>">
+		</div>
+		<div class="form-group">
+			<label for="pass">Password</label>
+			<input type="password" class="form-control" id="pass" placeholder="Password" name="pass" 
+				value="<?php echo isset($_REQUEST['pass']) ? $_REQUEST['pass'] : ''; ?>">
+		</div>
+		<button type="submit" class="btn btn-default" name="action" value="login">Submit</button>
+	</form>
+</div>
 
 <?php include('tpl' . DIRECTORY_SEPARATOR . 'footer.tpl.html'); ?>
