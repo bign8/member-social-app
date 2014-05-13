cb975a74daebd7ca1d13364bb2e471109cc807e4
bign8 <big.nate.w@gmail.com>
2014-05-13 08:42:00 -0600

    IE: adding IE=edge meta junk

--- reset.php
+++ reset.php
@@ -7,6 +7,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 	<title>ELA - App: Reset Password</title>
 	<link href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet">
